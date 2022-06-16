f0ac3bf43bb45c692f1eca47d390c0b17c652009
alsrua7222 <alsrua0531@gmail.com>
2022-06-16 21:43:22 +0900

    initalize Enum

---
+++ Translator/translators/TransEnumIdx.py
@@ -0,0 +1,12 @@
+# 1 = char, string
+# 2 = short
+# 3 = int, float
+
+class TransEnumIdx:
+    def __init__(self) -> None:
+        self.char = 1
+        self.string = 1
+        self.short = 2
+        self.int = 3
+        self.float = 3
+        return
